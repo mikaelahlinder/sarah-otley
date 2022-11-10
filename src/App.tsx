b8f49040457c69d79ee5c7cd2f802ef2ac94c575
Mikael Ahlinder <mikael.ahlinder@akindgroup.com>
2022-11-10 09:17:03 +0100

    font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import styled from "@emotion/styled"
 import React from "react"
 
+const Header = styled.div`
+  background-color: #000;
+  color: #fff;
+  font-family: "baskerville-poster-pt";
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -3.5px;
+  font-size: 115px;
+  height: 100vh;
+  padding: 20px;
+`
+
 const App: React.FC<{}> = () => {
-  return <div>Hello</div>
+  return (
+    <>
+      <Header>Hi there!</Header>
+    </>
+  )
 }
 
 export default App
